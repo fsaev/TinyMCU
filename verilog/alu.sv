18da8c2fa9dbcdcc5180afce6ab15d75f14f7fa1
Fsaev <fredrik@saevland.com>
2024-02-06 00:21:00 +0100

    Fix carry in the ALU, which is the 9th bit, not the 8th

--- verilog/alu.sv
+++ verilog/alu.sv
@@ -4,11 +4,15 @@
 
 `include "global_defines.sv"
 
-module alu(input clk, input reset, input wire [7:0] a, input wire [7:0] b, input wire [7:0] mode, output reg [7:0] out, output reg carry, output reg zero);
+module alu(input clk, input reset, input wire [7:0] a, input wire [7:0] b, input wire [7:0] mode, output [7:0] out, output reg carry, output reg zero);
 
     // ALU mode is persistent until pipeline resets at the beginning of a new ALU operation
     // This persistence is used for when doing conditional jumps
+    reg [8:0] sum_reg; // Append 1 bit for carry
     reg [7:0] alu_mode;
+
+    assign out = sum_reg[7:0];
+
     always_ff @(posedge clk) begin
         if(mode != `ALU_NON)
             alu_mode <= mode;
@@ -20,47 +24,53 @@ module alu(input clk, input reset, input wire [7:0] a, input wire [7:0] b, input
     begin
         case (alu_mode)
             `ALU_ADD: begin // ADD
-                out = a + b;
-                carry = (out[7] == 1'b1);
+                sum_reg = a + b;
+                carry = (sum_reg[8] == 1'b1);
                 zero = (out == 8'h00);
             end
             `ALU_SUB: begin // SUB
-                out = a - b;
-                carry = (out[7] == 1'b1);
+                sum_reg = a - b;
+                carry = (sum_reg[8] == 1'b1);
                 zero = (out == 8'h00);
             end
             `ALU_AND: begin // AND
-                out = a & b;
+                sum_reg[8] = 1'b0;
+                sum_reg[7:0] = a & b;
                 carry = 1'b0;
                 zero = (out == 8'h00);
             end
             `ALU_OR: begin // OR
-                out = a | b;
+                sum_reg[8] = 1'b0;
+                sum_reg[7:0] = a | b;
                 carry = 1'b0;
                 zero = (out == 8'h00);
             end
             `ALU_XOR: begin // XOR
-                out = a ^ b;
+                sum_reg[8] = 1'b0;
+                sum_reg[7:0] = a ^ b;
                 carry = 1'b0;
                 zero = (out == 8'h00);
             end
             `ALU_NOT: begin // NOT
-                out = ~a;
+                sum_reg[8] = 1'b0;
+                sum_reg[7:0] = ~a;
                 carry = 1'b0;
                 zero = (out == 8'h00);
             end
             `ALU_SHL: begin // SHL
-                out = a << 1;
-                carry = a[7];
+                sum_reg[8] = 1'b0;
+                sum_reg[7:0] = a << 1;
+                carry = sum_reg[8];
                 zero = (out == 8'h00);
             end
             `ALU_SHR: begin // SHR
-                out = a >> 1;
-                carry = a[0];
+                sum_reg[8] = 1'b0;
+                sum_reg[7:0] = a >> 1;
+                carry = a[0]; 
                 zero = (out == 8'h00);
             end
             default: begin
-                out = 8'h00;
+                sum_reg = 9'h000;
                 carry = 1'b0;
                 zero = 1'b0;
             end
